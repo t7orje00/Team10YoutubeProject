a3bb9e68d1758f459a8b3a4471a45818ebb797be
Jesse Orre <t7orje00@students.oamk.fi>
2019-10-20 16:24:30 +0300

    first commit

---
+++ app/src/main/java/com/example/team10project/Config.java
@@ -0,0 +1,10 @@
+package com.example.team10project;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyD7nW-IXkiV9Qvr2T2w6HC5HiXVmYRYCrI";
+
+}
